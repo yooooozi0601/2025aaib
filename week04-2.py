643ad110e5979a144a61b1f9a08b9863f64fde27
yooooozi0601 <0933138398yoyo@gmail.com>
2025-03-12 11:55:55 +0800

    week04

---
+++ week04-2.py
@@ -0,0 +1,10 @@
+#week04-2.py
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        pos=0
+        neg=0
+        for i in range(len(nums)):
+            if nums[i] > 0:pos +=1
+            if nums[i] < 0:neg +=1
+
+        return max(pos, neg)
