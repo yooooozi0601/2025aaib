62b07766ac2871d540dbf0a41e11a13a71675cfd
yooooozi0601 <0933138398yoyo@gmail.com>
2025-03-26 11:55:01 +0800

    week06

---
+++ week06-2.cpp
@@ -0,0 +1,22 @@
+///week06-2cpp
+///YKL
+# include <stdio.h>
+# include <math.h>
+
+int main()
+{
+
+    int a, b;
+    while(1){
+        scanf("%d%d", &a, &b);
+        if(a==0 && b==0)break;
+        int aa = sqrt(a), bb = sqrt(b);
+        int ans=0;
+        for(int i=aa; i<=bb; i++){
+            if(a<=i*i && i*i<=b){
+                ans++;
+            }
+        }
+        printf("%d\n", ans);
+    }
+}
