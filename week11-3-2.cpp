f04aa44aa25f67b620352e0aa6cf30c008f92799
yooooozi0601 <0933138398yoyo@gmail.com>
2025-04-30 12:00:00 +0800

    week11

---
+++ week11-3-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int f(int a,int b){
+  int ans=0
+  while(n>0){
+  	if(n%10>ans) ans=n%10
+  	n=n/10;
+  }
+  return ans;
+}
+int main(){
+  int a, b;cin>>a>>b;
+  cout<<f(a,b)<<endl;
+  return 0;
+}
