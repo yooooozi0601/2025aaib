d33b25a4ff7c81c284a9af7f86cdf9e2a81413b5
yooooozi0601 <0933138398yoyo@gmail.com>
2025-05-14 11:46:44 +0800

    week13

---
+++ week13-4.cpp
@@ -0,0 +1,19 @@
+///week13-4.cpp
+///leetcode
+class Solution {
+public:
+    int a[200000]={};
+    int helper(int t){
+        if(a[t]>0) return a[t];
+        if(t<26) return 1;
+        a[t]=(helper(t-26)+helper(t-26+1))%1000000007;
+        return a[t];
+    }
+    int lengthAfterTransformations(string s, int t) {
+        int ans=0;
+        for(char c : s){//c++的語法,可以把每個數字取出來
+            ans=(ans+helper(t+c-'a'))%1000000007;
+        }
+        return ans;
+    }
+};
