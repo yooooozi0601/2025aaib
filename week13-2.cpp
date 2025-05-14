4ab7058d06d2ed3d346d647791ec0afefcc367c9
yooooozi0601 <0933138398yoyo@gmail.com>
2025-05-14 11:46:10 +0800

    week13

---
+++ week13-2.cpp
@@ -0,0 +1,13 @@
+///week13-2.cpp
+///衡程そ计,よ獽だだダ
+#include <stdio.h>
+int main()
+{
+    int a=51,b=68;
+    int ans=1;
+    for(int i=1;i<=a; i++){
+        if(a%i==0 && b%i==0) ans=i;
+    }
+    printf("т%d %d程そ计: %d\n",a,b,ans);
+    printf("%d/%d だ,跑Θ %d/%d\n",a,b,a/ans,b/ans);
+}
