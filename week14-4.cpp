cc11ceda75c8c8adebee9da08980afd159bed48c
yooooozi0601 <0933138398yoyo@gmail.com>
2025-05-21 11:46:21 +0800

    week14

---
+++ week14-4.cpp
@@ -0,0 +1,24 @@
+///week14-4.cpp
+//73.set matrix zeroes
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int M = matrix.size(),N=matrix[0].size();
+        vector<int> left(M),up(N);
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                if(matrix[i][j]==0){
+                    left[i]=7;
+                    up[j]=7;
+                }
+            }
+        }
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                if(left[i]==7 || up[j]==7)matrix[i][j]=0;
+            }
+        }
+
+
+    }
+};
